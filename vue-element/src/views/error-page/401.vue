90e8d6f8bc8579bc3073af64fa2bdb17578722cf
Thai Bui Minh <thai.buiminh@ncc.asia>
2020-07-30 11:40:03 +0700

    Update 401 error page

--- vue-element/src/views/error-page/401.vue
+++ vue-element/src/views/error-page/401.vue
@@ -5,34 +5,24 @@
       class="back-btn"
       @click="back"
     >
-      返回
+      Back
     </el-button>
     <el-row>
       <el-col :span="12">
         <h1 class="text-jumbo text-ginormous">
           Oops!
         </h1>
-        gif来源<a
-          href="https://zh.airbnb.com/"
-          target="_blank"
-        >airbnb</a> 页面
-        <h2>你没有权限去该页面</h2>
-        <h6>如有不满请联系你领导</h6>
         <ul class="list-unstyled">
-          <li>或者你可以去:</li>
           <li class="link-type">
             <router-link to="/dashboard">
-              回首页
+              Back to home
             </router-link>
-          </li>
-          <li class="link-type">
-            <a href="https://github.com/armour">随便看看</a>
           </li>
           <li>
             <a
               href="#"
               @click.prevent="dialogVisible=true"
-            >点我看图</a>
+            >Open Dialog</a>
           </li>
         </ul>
       </el-col>
@@ -48,7 +38,7 @@
     </el-row>
     <el-dialog
       :visible.sync="dialogVisible"
-      title="随便看"
+      title="Dialog"
     >
       <img
         :src="ewizardClap"
@@ -89,7 +79,7 @@ export default class extends Vue {
   .back-btn {
     background: #008489;
     color: #fff;
-    border: none!important;
+    border: none !important;
   }
 
   .some-gif {
